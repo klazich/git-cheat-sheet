9600c58fd4860b69d30c6014ea0fed00cb9c56df
Kevin Lazich <dev@kevinlazich.com>
2018-07-23 08:19:41 -0400

    chore: Add main.js as main entry to app

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 
-console.log(' ******** here I am ********')
+try {
+  const app = require('./lib/app')
+} catch (err) {
+  console.error('The /lib directory was not loaded. Did it get built?')
+  process.exit(1)
+}
+app.listen(port, () => {
+  console.log(`Server started ~ listening on 3001`)
+})
