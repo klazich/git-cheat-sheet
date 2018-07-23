717a1ba23d988622a2025d1ba1402f40261a6f4c
Kevin Lazich <dev@kevinlazich.com>
2018-07-23 15:34:27 -0400

    chore: Update express initializations

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 import 'dotenv/config' // required for Dotenv
 
-import { join } from 'path'
-
 import express, { json, urlencoded } from 'express'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
@@ -11,56 +9,52 @@ import passport from 'passport'
 import indexRouter from './routes/index'
 import usersRouter from './routes/users'
 import apiRouter from './routes/api'
+import config from '../config'
+const {
+  app: { port },
+} = config
 
+// INITIALIZE A NEW EXPRESS APP
 var app = express()
 
-app.use(logger('dev'))
-app.use(json())
-app.use(urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(cors())
-// app.use(express.static(join(__dirname, 'public')))
-// app.use(express.static(path.join(__dirname, 'build')))
+// LOAD EXPRESS MIDDLEWARE
+app.use(logger('dev')) // request logging with morgan
+app.use(json()) // content-type: application/json
+app.use(urlencoded({ extended: false })) // content-type: application/x-www-form-urlencoded
+app.use(cookieParser()) // read cookies
+app.use(cors()) // cross-origin resource sharing middleware
 
+// ASSIGN ROUTERS
 // app.use('/', indexRouter)
-// app.use('/users', usersRouter)
+app.use('/me', usersRouter) // JWT secured
 app.use('/api', apiRouter)
 
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found')
-//   err.status = 404
-//   next(err)
-// })
-
-// error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message
-//   res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-//   // render the error page
-//   res.status(err.status || 500)
-//   res.render('error')
-// })
-
-// Setup mongoose and connect to MongoDB
+// LOAD MONGOOSE
 import './database'
-// Setup Passport strategies
-import './passport'
 
+// LOAD PASSPORT & INITIALIZE
+import './passport'
 app.use(passport.initialize())
 
-// CHECK //////////////////////////////////////////////////////////////////////
-app.get('/checking', function(req, res) {
-  res.json({
-    Tutorial: 'Welcome to the Node express JWT Tutorial',
-  })
+app.get('/up', function(req, res) {
+  res.status(200).json({ isUp: true })
 })
-///////////////////////////////////////////////////////////////////////////////
 
-const PORT = process.env.APP_PORT
+// Catch 404 and forward to error handler.
+app.use(function(req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
+// Send errors as json and not html.
+app.use(function(err, req, res, next) {
+  console.error(err) // Log the error.
+  res.status(err.status || 500).json({ error: err.toString() })
+})
+
+// START THE SERVER
+const PORT = port
 app.listen(PORT, function() {
   console.log('-- Server is running on Port', PORT)
 })
