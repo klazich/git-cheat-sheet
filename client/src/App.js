ab52bb7d41000a8b803f43f22ea489313a569f82
Kevin Lazich <dev@kevinlazich.com>
2018-07-25 18:33:44 -0400

    style: Small changes

--- client/src/App.js
+++ client/src/App.js
@@ -7,46 +7,21 @@ import {
   Redirect,
 } from 'react-router-dom'
 
-import logo from './logo.svg'
+// import logo from './logo.svg'
 import './App.css'
 
-import Home from './components/Home'
-import Hello from './components/Hello'
-import About from './components/About'
-import Stuff from './components/Stuff'
+import CheatSheet from './components/CheatSheet'
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <div className="container">
-            <ul>
-              <li>
-                <Link to="/hello">Hello</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/stuff">Stuff</Link>
-              </li>
-            </ul>
-            <hr />
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/hello" component={Hello} />
-              <Route
-                path="/hello/goodmorning"
-                render={() => {
-                  return <h1>Goodmorning</h1>
-                }}
-              />
-              <Route path="/stuff" component={Stuff} />
-            </Switch>
-          </div>
-        </div>
-      </Router>
+      <div>
+        <Router>
+          <Switch>
+            <Route path="/" component={CheatSheet} />
+          </Switch>
+        </Router>
+      </div>
     )
   }
 }
